5ef64f72b19cb069f2eea68016479e380810f3ac
sabariganesan <sabariganesan01@gmail.com>
2022-01-07 10:29:04 +0530

    day 2-sample test for pulling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Content from './component/content/Content'
 import Head from './component/header/Header'
 
 function App() {
   return (
     <div>
       <Head/>
+      <Content/>
     </div>
   )
 }
